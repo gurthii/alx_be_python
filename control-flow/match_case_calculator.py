723e876600f50a29b0a26b3621ab2c5663beb63b
Joseph G <josef.emu@gmail.com>
2025-09-20 11:52:06 +0300

    Renamed variable according to specs

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,14 +1,14 @@
 num1, num2 = int(input("Enter the first number: ")), int(input("Enter the second number: "))
-operation_type = input("Choose the operation (+, -, *, /): ")
+operation = input("Choose the operation (+, -, *, /): ")
 
-match operation_type:
-	case operation_type if operation_type == "+":
+match operation:
+	case operation if operation == "+":
 		print(f"The result is [{num1 + num2}].")
-	case operation_type if operation_type ==  "-":
+	case operation if operation ==  "-":
 		print(f"The result is [{num1 - num2}].")
-	case operation_type if operation_type == "*":
+	case operation if operation == "*":
 		print(f"The result is [{num1 * num2}].")
-	case operation_type if operation_type == "/":
+	case operation if operation == "/":
 		try:
 			num1 / num2
 			print(f"The result is [{num1 / num2}].")
